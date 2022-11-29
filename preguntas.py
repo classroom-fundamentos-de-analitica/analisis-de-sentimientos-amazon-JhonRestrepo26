07828c9c55000035af710251c2c07f25fe29a236
Jhon Alexander Restrepo Henao <112440657+JhonRestrepo26@users.noreply.github.com>
2022-11-28 19:39:41 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -21,22 +21,22 @@ def pregunta_01():
     # Lea el archivo `amazon_cells_labelled.tsv` y cree un DataFrame usando pandas.
     # Etiquete la primera columna como `msg` y la segunda como `lbl`. Esta función
     # retorna el dataframe con las dos columnas.
-    df = ____(
-        ____,
-        sep=____,
-        header=____,
-        names=____,
+    df = pd.read_csv(
+        '/tmp/amazon_cells_labelled.tsv',
+        sep='\t',
+        header=0,
+        names=['msg','lb1'],
     )
 
     # Separe los grupos de mensajes etiquetados y no etiquetados.
-    df_tagged = ____[____["____"].____()]
-    df_untagged = ____[____["____"].____()]
+    df_tagged = df[df["lb1"].notnull()]
+    df_untagged = df[df["lb1"].isnull()]
 
-    x_tagged = ____["____"]
-    y_tagged = ____["____"]
+    x_tagged = df_tagged["msg"]
+    y_tagged = df_tagged["lb1"]
 
-    x_untagged = ____["____"]
-    y_untagged = ____["____"]
+    x_untagged = df_untagged["msg"]
+    y_untagged = df_untagged["lb1"]
 
     # Retorne los grupos de mensajes
     return x_tagged, y_tagged, x_untagged, y_untagged
